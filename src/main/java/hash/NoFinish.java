557fb44f7eb0803aafb7d42e94f9116461d7dee8
cvivis <hsy0754@naver.com>
2022-10-27 09:12:45 +0900

    주석 추가

--- src/main/java/hash/NoFinish.java
+++ src/main/java/hash/NoFinish.java
@@ -6,29 +6,19 @@ public class NoFinish {
     public String solution(String[] participant, String[] completion) {
         String str = "";
     HashMap<String,Integer> participantCount = new HashMap<>();
-        for (int i = 0; i < participant.length; i++) {
+        for (int i = 0; i < participant.length; i++) { // 참가자의 이름을 키값으로 해쉬맵을 만들어 +1, 기본값은 0
             participantCount.put(participant[i],participantCount.getOrDefault(participant[i],0)+1);
 
         }
-        for(int i = 0; i < completion.length;i++){
+        for(int i = 0; i < completion.length;i++){ // 조회해서 완주했으면 key갑의 value -1
             participantCount.put(completion[i],participantCount.get(completion[i])-1);
         }
-        Set<String> keys = participantCount.keySet();
-            for(String key:keys){
+        Set<String> keys = participantCount.keySet(); // 참가자들의 키 값을 set으로 만들고
+            for(String key:keys){  // 키값을 조회해 get 으로 0 이 아닌 값을 판별해준다-> 0이 아닌것은 완주하지 못한것
                 if(participantCount.get(key)!=0){
                     str = key;
                 }
             }
-//        Iterator<Map.Entry<String, Integer>> iter = participantCount.entrySet().iterator();
-//
-//        while(iter.hasNext()){
-//            Map.Entry<String, Integer> entry = iter.next();
-//            if (entry.getValue() != 0){
-//                str = entry.getKey();
-//                break;
-//            }
-//        }
-
 
         return str;
     }

90161c67dcf1cc298338469ff5435ea28c2c840a
cvivis <hsy0754@naver.com>
2022-10-31 10:24:59 +0900

    프로그래머스 모의고사 - 문제 해결

--- src/main/java/bruteforce/PrepareTest.java
+++ src/main/java/bruteforce/PrepareTest.java
@@ -1,38 +1,52 @@
 package bruteforce;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class PrepareTest {
     public int[] solution(int[] answers) {
         int[] answer = {};
         int[] first = {1,2,3,4,5};
         int[] second = {2,1,2,3,2,4,2,5};
         int[] third = {3,3,1,1,2,2,4,4,5,5};
-        int firstStudentAnswerCnt = 0;
-        int secondStudentAnswerCnt = 0;
-        int thirdStudentAnswerCnt = 0;
+        int[] score = {0,0,0};
         for (int i = 0; i < answers.length; i++) {
             if(answers[i] == first[i%first.length]){
-                firstStudentAnswerCnt++;
+                score[0] = score[0]+1;
             }
             if(answers[i] == second[i%second.length]){
-                secondStudentAnswerCnt++;
+                score[1] = score[1]+1;
             }
             if(answers[i] == third[i%third.length]){
-                thirdStudentAnswerCnt++;
+                score[2] = score[2]+1;
             }
         }
-
-
-
-        System.out.println(firstStudentAnswerCnt);
-        System.out.println(secondStudentAnswerCnt);
-        System.out.println(thirdStudentAnswerCnt);
+        int max = Math.max(score[0],Math.max(score[1],score[2])); // 3개중 최대값 설정
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < score.length; i++) { // 최대값과 같으면 list에 추가
+            if(score[i]==max){
+                list.add(i+1);
+            }
+        }
+        answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) { // list -> array
+            answer[i] = list.get(i).intValue();
+            System.out.println( answer[i]);
+        }
 
         return answer;
     }
 
     public static void main(String[] args) {
-        int[]arr = {1,3,2,4,2};
+//        int[]arr = {1,3,2,4,2};
+        int[]arr = {1,2,3,4,5};
         PrepareTest prepareTest = new PrepareTest();
-        System.out.println(prepareTest.solution(arr));
+        int[] result = prepareTest.solution(arr);
+//        for (int i = 0; i < result.length; i++) {
+//            System.out.println(result[i]);
+//        }
+
     }
 }

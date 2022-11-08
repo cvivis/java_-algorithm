4c0d6486d9d5ecc1cbd723553dd3e34d4789c2b6
cvivis <hsy0754@naver.com>
2022-11-08 10:42:15 +0900

    [코드업] 이분탐색 답은 맞는데 시간초과

--- src/main/java/SimpleSeach/BinarySearch.java
+++ src/main/java/SimpleSeach/BinarySearch.java
@@ -1,29 +1,39 @@
 package SimpleSeach;
 
 import java.util.Scanner;
-
+/**
+3 7
+2 5 7
+ **/
 public class BinarySearch {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int find = sc.nextInt();
-        int[] nums = new int[]{1,2,3,4,5,6,7,8,9,10,11};
+
+    public int binarySearch(int[] nums, int find){
         int middle = nums.length/2;//중간값
         int start = 0;
         int end = nums.length-1;
         while(nums[middle] != find){//같은지 비교하기
             if(nums[middle]>find) {
-                end = middle;//인덱스 옮기기
+                end = middle-1;//인덱스 옮기기
             }
             else{
-                start = middle;//인덱스 옮기기
+                start = middle+1;//인덱스 옮기기
             }
             middle = (end + start)/2;//중간값
         }
-        System.out.println(middle);
 
+        return middle;
+    }
 
-
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int len = sc.nextInt();
+        int find = sc.nextInt();
+        int[] nums = new int[len];
+        for (int i = 0; i < len; i++) {
+            nums[i] = sc.nextInt();
+        }
+        BinarySearch bs = new BinarySearch();
+        System.out.println(bs.binarySearch(nums,find)+1);
 
     }
 }

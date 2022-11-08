f5a15456a7834969a31f3876ba504ec39b19b79a
cvivis <hsy0754@naver.com>
2022-11-08 10:56:47 +0900

    [코드업] 이분탐색 2083 조건 수정

--- src/main/java/SimpleSeach/BinarySearch.java
+++ src/main/java/SimpleSeach/BinarySearch.java
@@ -11,17 +11,22 @@ public class BinarySearch {
         int middle = nums.length/2;//중간값
         int start = 0;
         int end = nums.length-1;
-        while(nums[middle] != find){//같은지 비교하기
+
+        while(start <= end){//같은지 비교하기
             if(nums[middle]>find) {
                 end = middle-1;//인덱스 옮기기
             }
-            else{
+            else if(nums[middle]<find){
                 start = middle+1;//인덱스 옮기기
             }
+            else if (nums[middle] == find){
+                return middle;
+            }
+
             middle = (end + start)/2;//중간값
         }
 
-        return middle;
+        return -2;
     }
 
     public static void main(String[] args) {

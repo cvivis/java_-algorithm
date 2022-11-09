a45ade9e9e6feb7c7e2b6c11955142abe8643937
cvivis <hsy0754@naver.com>
2022-11-09 09:38:44 +0900

    SelectionSort 구현

--- src/main/java/selectionSort/SelectionSort.java
+++ src/main/java/selectionSort/SelectionSort.java
@@ -1,11 +1,12 @@
 package selectionSort;
 
+import java.util.Arrays;
+
 public class SelectionSort {
 
-    public int findMin(int[] arr){
-        int index = 0;
-        int min = arr[0];
-        for (int i = 0; i < arr.length; i++) {
+    public int findMin(int[] arr, int index){
+        int min = arr[index];
+        for (int i = index; i < arr.length; i++) {
             if(arr[i]<min){
                 min = arr[i];
                 index = i;
@@ -14,10 +15,25 @@ public class SelectionSort {
         return index;
     }
 
-    public static void main(String[] args) {
-        int[] arr = new int[]{11,7,72,9,10,223,111,23,7,4,39};
-        SelectionSort ss = new SelectionSort();
-        System.out.println(ss.findMin(arr));
+    public int[] swap(int[]arr , int index1 , int index2){
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+        return arr;
+    }
 
+    public void sort(int[] arr){
+        int swapIdx = 0;
+        for (int i = 0; i < arr.length; i++) {
+            swapIdx = findMin(arr,i);
+            arr = swap(arr,i,swapIdx);
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{11,7,72,9,10,223,111,23,8,4,39};
+        SelectionSort ss = new SelectionSort();
+        ss.sort(arr);
     }
 }

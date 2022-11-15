e8edc72c314c344ec284abd7d37f382357c67356
cvivis <hsy0754@naver.com>
2022-11-15 10:39:53 +0900

    퀵소트 구현은됨...

---
+++ src/main/java/quickSort/quickSort.java
@@ -0,0 +1,54 @@
+package quickSort;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class quickSort {
+
+    // 재귀
+    public List<Integer> sort(List<Integer> arr){
+//        if(arr.size() <= 1)return arr;
+        List<Integer> low = new ArrayList<>();
+        List<Integer> middle = new ArrayList<>();
+        List<Integer> high = new ArrayList<>();
+        int pivot = arr.size()/2;
+        for (int i = 0; i < arr.size(); i++) {
+            if(arr.get(pivot) > arr.get(i)){
+                low.add(arr.get(i));
+            }
+            else if(arr.get(pivot) < arr.get(i)){
+                high.add(arr.get(i));
+            }
+            else{
+                middle.add(arr.get(i));
+            }
+        }
+        List<Integer> list = new ArrayList<>();
+        List<Integer> lowList = new ArrayList<>();
+        List<Integer> highList = new ArrayList<>();
+        if(low.size()>1){
+            lowList = sort(low);
+        }
+        if(high.size()>1){
+            highList = sort(high);
+        }
+
+        list.addAll(lowList);
+        list.addAll(middle);
+        list.addAll(highList);
+         
+        return list ;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {20,18,5,19,5,25,40,50};
+        List<Integer> list = new ArrayList<>();
+        for(int num:arr){
+            list.add(num);
+        }
+        quickSort q = new quickSort();
+        System.out.println( q.sort(list));
+
+    }
+}

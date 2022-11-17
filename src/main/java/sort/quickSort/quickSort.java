b14acc8e41a31cf15236fe09cc389ee9aed1a23a
cvivis <hsy0754@naver.com>
2022-11-17 10:40:31 +0900

    퀵소트 - 종료조건 변경

--- src/main/java/sort/quickSort/quickSort.java
+++ src/main/java/sort/quickSort/quickSort.java
@@ -53,38 +53,76 @@ public class quickSort {
     public int[] sort2(int[] arr,int low, int high) {
         System.out.println("low: "+ low);
         System.out.println("high: "+ high);
-        if(high-low<=0)return arr; // 사이즈가 1이면 종료
-        int pivot = (low + high+1)/2;
+
+        int pivot = (low + high)/2;
         int lowIdx = low;
         int highIdx = high; // size - 1
         while (lowIdx <= highIdx) {
-            while (arr[lowIdx] < arr[pivot]) lowIdx++;
-            while (arr[highIdx] > arr[pivot]) highIdx--;
-//            System.out.println("low: "+ lowIdx);
-//            System.out.println("high: "+ highIdx);
-            swap(arr, lowIdx, highIdx);
-            lowIdx++;
-            highIdx--;
-            System.out.println(Arrays.toString(arr));
+//            System.out.println(Arrays.toString(arr));
+            while (arr[lowIdx] < arr[pivot]) lowIdx+=1;
+            while (arr[highIdx] > arr[pivot]) highIdx-=1;
+//            if(lowIdx <= highIdx){
+                swap(arr, lowIdx, highIdx);
+                lowIdx+=1;
+                highIdx-=1;
+//            }
         }
-
-        sort2(arr,low,highIdx);
-        sort2(arr,lowIdx,high);
+        if(low<highIdx)sort2(arr,low,highIdx);
+        if(lowIdx<high)sort2(arr,lowIdx,high);
         return arr;
     }
+
+
+    //해답 코드
+    public int[] sort3(int[] arr, int startIdx, int endIdx) {
+        int leftIdx = startIdx;
+        int rightIdx = endIdx;
+        int pivot = arr[(startIdx + endIdx) / 2];
+        // 언제까지 반복되는지? leftIdx == rightIdx일 때도 아래 로직이 반복됨
+        // ex) leftIdx = 3, rightIdx = 3일 때도
+        // 왼쪽과 오른쪽 각각 교환할 index를 정하는 부분 pivot까지 올 수도 있다.
+        while(leftIdx <= rightIdx){
+            while(arr[leftIdx] < pivot) leftIdx += 1;   // <=아닙니다.
+            while(arr[rightIdx] > pivot) rightIdx -= 1;   // <=아닙니다.
+            // leftIdx = 4, rightIdx = 7 그대로 왜 냐하면 25는 교환 대상이기 때문입니다.
+
+            if(leftIdx <= rightIdx){
+                int temp = arr[leftIdx];
+                arr[leftIdx] = arr[rightIdx];
+                arr[rightIdx] = temp;
+                leftIdx += 1;
+                rightIdx -= 1;
+            }
+            System.out.printf("leftIdx:%d rightIdx:%d\n", leftIdx, rightIdx);
+            System.out.println(Arrays.toString(arr));
+        }
+        // 교환이 모두 끝나면 왼쪽, 오른쪽 두 그룹으로 나누어 지면 됩니다.
+        // 20, 18, 5, 19, 25, 5, 50, 40
+
+        if(startIdx < rightIdx) sort3(arr, startIdx, rightIdx);
+        if(leftIdx < endIdx) sort3(arr, leftIdx, endIdx);
+
+        return arr;
+    }
+
+
+
 
     public int[] partition(int[] arr,int a, int b){
         return Arrays.copyOfRange(arr,a,b);
     }
         public static void main (String[]args){
-            int[] arr = {20, 18, 5, 19, 40, 50, 5, 25};
+            int[] arr = {20, 18, 5, 1, 40, 19, 5, 25};
+            int[] arr2 = {20, 18, 5, 1};
             List<Integer> list = new ArrayList<>();
             for (int num : arr) {
                 list.add(num);
             }
             quickSort q = new quickSort();
 //        System.out.println( q.sort(list));
-            System.out.println(Arrays.toString(q.sort2(arr,0,arr.length-1)));
+            System.out.println(Arrays.toString(q.sort3(arr2,0,arr2.length-1)));
+
+            System.out.println(Arrays.toString(q.sort2(arr2,0,arr2.length-1)));
 
         }
     }

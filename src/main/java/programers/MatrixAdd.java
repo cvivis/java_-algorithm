caf86b4d42e77d4f61d37769627ba8fe9fd28773
cvivis <hsy0754@naver.com>
2022-11-18 09:58:54 +0900

    [프로그래머스] 행렬의 덧셈

---
+++ src/main/java/programers/MatrixAdd.java
@@ -0,0 +1,23 @@
+package programers;
+
+import java.util.Arrays;
+
+public class MatrixAdd {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr1[0].length];
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr1[i].length; j++) {
+                answer[i][j] = arr1[i][j]+arr2[i][j];
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        MatrixAdd matrixAdd = new MatrixAdd();
+        int[][] arr1 = {{1,2},{2,3}};
+        int[][] arr2 = {{3,4},{5,6}};
+
+
+    }
+}

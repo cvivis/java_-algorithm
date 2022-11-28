9d5c7581d0fcd1f3eb6bb0619cbdf674b29fa2bd
cvivis <hsy0754@naver.com>
2022-11-28 10:33:01 +0900

    알파벳 길이 설정

--- src/main/java/permutation/CombinationOfAlphabet.java
+++ src/main/java/permutation/CombinationOfAlphabet.java
@@ -2,15 +2,18 @@ package permutation;
 
 public class CombinationOfAlphabet {
 
-    String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    static String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     public static void main(String[] args) {
         CombinationOfAlphabet combination = new CombinationOfAlphabet();
-        combination.printAlpabet();
+        combination.printAlphabet();
         combination.printTwoAlpabet();
+        combination.alphabetRecur(0);
+        printAllAlphabet("",3);
+
     }
 
-    public void printAlpabet(){
+    public void printAlphabet(){
         for (int i = 0; i < alphabet.length(); i++) {
             System.out.println(alphabet.charAt(i));
         }
@@ -20,6 +23,27 @@ public class CombinationOfAlphabet {
             for (int j = 0; j < alphabet.length(); j++) {
                 System.out.println(alphabet.charAt(i)+ " "+alphabet.charAt(j));
             }
+        }
+    }
+
+    public int alphabetRecur(int num){
+        if(num == alphabet.length()-1)return 0;
+        System.out.println(alphabet.charAt(num));
+        return alphabetRecur(num+1);
+    }
+
+    public void alphabetTwoRecur(char c,String prefix){
+        if(c > 'Z')return ;
+        System.out.println(prefix + c);
+        alphabetTwoRecur((char)(c+1),prefix);
+    }
+    public static void printAllAlphabet(String prefix,int num) {
+        if (prefix.length() == num) {
+            System.out.println(prefix);
+            return;
+        }
+        for (char c = 'A'; c <= 'Z' ; c++) {
+            printAllAlphabet(prefix + c,num);
         }
     }
 }
